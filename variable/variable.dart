6628aa6482b5451a1f2ca206814c689a29ed6f7a
mhmadamrii <interceptorghost4@gmail.com>
2022-10-23 17:35:48 +0700

    variable & type of data

---
+++ variable/variable.dart
@@ -0,0 +1,21 @@
+void main() {
+  // variable
+  String firstName = "Muhammad";
+  bool isLogin = false;
+
+  print(firstName);
+  print(isLogin);
+
+  // tipe data
+  String hello = "Hello World";
+  num myNumber = 45;
+  int myIntNumber = 39;
+  double myDoNumber = 3.14;
+
+  // string interpolation
+  String first = "John";
+  String last = "Doe";
+  String fullName = "$first $last";
+
+  print("My fullname is $fullName");
+}
