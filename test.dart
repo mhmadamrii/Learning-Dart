adafb5d314c48dd36ca7e8369176ee52818f3b31
mhmadamrii <interceptorghost4@gmail.com>
2022-10-27 14:30:00 +0700

    test dengan function

---
+++ test.dart
@@ -0,0 +1,18 @@
+class Person {
+  String name = "Guest";
+}
+
+void main() {
+  var person1 = Person();
+
+  myFunc();
+  print(person1.name);
+}
+
+void myFunc() {
+  var angka1 = 50;
+  var angka2 = 40;
+
+  final int hasil = angka1 + angka2;
+  print(hasil);
+}
