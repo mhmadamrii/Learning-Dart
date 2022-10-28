10b1bf604649814d784be0b47c69ae5278c88e51
mhmadamrii <interceptorghost4@gmail.com>
2022-10-28 14:16:05 +0700

    adding fucking shit

--- test.dart
+++ test.dart
@@ -1,18 +1,10 @@
-class Person {
-  String name = "Guest";
-}
-
 void main() {
-  var person1 = Person();
+  int total = 0;
+  int n = 100;
 
-  myFunc();
-  print(person1.name);
-}
+  for (int i = 1; i <= n; i++) {
+    total = total + i;
+  }
 
-void myFunc() {
-  var angka1 = 50;
-  var angka2 = 40;
-
-  final int hasil = angka1 + angka2;
-  print(hasil);
+  print("Total is $total");
 }
