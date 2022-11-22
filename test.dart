a528f2d544b2658334809f92a3a9aee99cd138b4
mhmadamrii <interceptorghost4@gmail.com>
2022-11-22 07:44:49 +0700

    practice

--- test.dart
+++ test.dart
@@ -1,1 +1,26 @@
-import "dart:io";
+void main() {
+  User user = User();
+  user.printing();
+  user.printingAgain("Jonathan");
+
+  Hero hero = Hero("Harley", 5000);
+  print(hero.assassin);
+}
+
+class User {
+  void printing() {
+    print("Hello I am from class");
+  }
+
+  void printingAgain(String userName) {
+    print(userName);
+  }
+}
+
+class Hero {
+  String? assassin;
+
+  Hero(String name, int strength) {
+    this.assassin = name;
+  }
+}
